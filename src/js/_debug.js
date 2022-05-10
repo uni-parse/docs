8741f57c21f8c77c5b577fc0a24d395ad835a44f
UniParse <103385379+TheUniParse@users.noreply.github.com>
2022-05-10 22:32:11 +0100

    update docs project after viteJS

--- src/js/_debug.js
+++
@@ -1,1 +0,0 @@
-function a(a){switch(a){case 1:void 0;break;case"hello":void 0;break;case a>=2:void 0;break}}a(1);void 0;
--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-function a(choice) {
-  switch (choice) {
-    case 1: console.log(1); break;
-    case 'hello': console.log('hello'); break;
-    case choice >= 2: console.log(`${choice}≥2`); break;
-  }
-};
-a(1);
-console.log('hi')
